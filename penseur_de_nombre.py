55557d284076039f53a582f0fd498aad89fef4f0
Alexis <a.quentin.08@gmail.com>
2019-12-31 11:59:05 +0100

    1st commit

---
+++ penseur_de_nombre.py
@@ -0,0 +1,30 @@
+import random
+
+# score = essai
+essai = 0
+rejouer = ("oui")
+while rejouer != "non" :
+    nombre_joueur = 0
+    nombre = random.randint(1,99)
+    # nombre = 46
+    while nombre != nombre_joueur :
+        essai += 1
+        result = (nombre - nombre_joueur)
+        nombre_joueur = int(input("entrer le nombre :"))
+        if nombre == nombre_joueur :
+            print("c'est gagner")
+        elif nombre_joueur < nombre :
+            if (nombre - nombre_joueur) <= 1 :
+                print ("tu es brulant")
+            elif (nombre - nombre_joueur) <= 5 :
+                print ("tu chaufe")
+            else :
+                print("trop petit")
+        elif nombre_joueur > nombre :
+            if (nombre - nombre_joueur) <= 0 :
+                print ("tu refroidis")
+            else :
+                print("trop grand")
+    print ("Ton score est de {}".format(essai))
+    # print (score)
+    rejouer = input("veux tu rejouer ?")
